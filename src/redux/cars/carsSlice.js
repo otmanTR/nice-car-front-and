15252cc4827fcbccf2afeb690a191f167da0a00b
alicemirigo92 <alicemirigo92@gmai.com>
2023-06-22 11:47:38 +0300

    Add changes

--- src/redux/cars/carsSlice.js
+++ src/redux/cars/carsSlice.js
@@ -2,9 +2,32 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const initialState = {
-
 };
-
+const url = 'http://127.0.0.1:3000/api/v1/cars';
+export const getCars = createAsyncThunk('getCars', async () => {
+  const response = await axios.get(url);
+  const carData = response.data;
+  const cars = [];
+  Object.keys(carData).forEach((key) => {
+    const car = carData[key];
+    cars[car.id] = {
+      id: car.id,
+      name: car.name,
+      image: car.image,
+      model: car.model,
+      price: car.price,
+    };
+  });
+  return cars;
+});
+export const deleteCar = createAsyncThunk('deleteCar', async (id) => {
+  try {
+    const response = await axios.delete(`${url}/${id}`);
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
 export const carsSlice = createSlice({
   name: 'cars',
   initialState,
@@ -21,8 +44,6 @@ export const carsSlice = createSlice({
         newStatus: payload,
       }));
   },
-
 });
-
 export const { carsFilter } = carsSlice.actions;
 export default carsSlice.reducer;
